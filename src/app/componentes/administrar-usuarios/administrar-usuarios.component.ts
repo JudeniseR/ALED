1a71c1feefca2f6e0adf93d8c483282a6dfb65c8
JudeniseR <julietadeniserojas@gmail.com>
2025-05-10 22:38:51 -0300

    subir componentes

---
+++ src/app/componentes/administrar-usuarios/administrar-usuarios.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-administrar-usuarios',
+  imports: [],
+  templateUrl: './administrar-usuarios.component.html',
+  styleUrl: './administrar-usuarios.component.css'
+})
+export class AdministrarUsuariosComponent {
+
+}
